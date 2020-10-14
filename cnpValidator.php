cf5ce808c3f82fd4ff261131ac696ea348195d66
Alex Barbu <barbualex.tgv@gmail.com>
2020-10-14 21:02:47 +0300

    all done

--- cnpValidator.php
+++ cnpValidator.php
@@ -1,44 +1,22 @@
 <?php
 
-# Create our class
-// $router = new Router;
-// $router->post('/validator', function($request) {
-//     $body = $request->getBody();
-//     return $body;
-// });
-
 function cnpValidator($cnp) {
     $len = checkLength($cnp);
-
-    if($len) {
-        echo "<p>Length is OK</p>";
-    } else {
-        echo "<p>Length is not OK</p>";
-    }
+    // echo 'Length is '.($len ? '':'not ').'valid <br>';
 
     $lastDigit = checkLastDigit($cnp);
-
-    if($lastDigit) {
-        echo "<p>Last digit is OK</p>";
-    } else {
-        echo "<p>Last digit is not OK</p>";
-    }
+    // echo 'Last digit is '.($lastDigit ? '':'not ').'valid <br>';
 
     $birth = checkBirth($cnp);
-
-    if($birth) {
-        echo "<p>Birht date is OK</p>";
-    } else {
-        echo "<p>Birht date is not OK</p>";
-    }
+    // echo 'Birth is '.($birth ? '':'not ').'valid <br>';
 
     $county = checkCounty($cnp);
+    // echo 'County is '.($county ? '':'not ').'valid <br>';
+   
+    // echo 'CNP is '.(($len and $lastDigit and $birth and $county) ? '':'not ').'valid <br>';
 
-    if($county) {
-        echo "<p>County is OK</p>";
-    } else {
-        echo "<p>County is not OK</p>";
-    }
+    return $len and $lastDigit and $birth and $county;
+
 }
 
 function checkLength($cnp) {
@@ -51,10 +29,10 @@ function checkLength($cnp) {
 
 function checkLastDigit($cnp) {
     
-    $calc = array( 2 , 7 , 9 , 1 , 4 , 6 , 3 , 5 , 8 , 2 , 7 , 9 );
+    $calc = array(2,7,9,1,4,6,3,5,8,2,7,9);
     $sum = 0;
     $raw_cnp = str_split($cnp);
-    $checker = 0;
+    $lastDigit = 0;
     
     for($n=0 ; $n<13 ; $n++) {
         
@@ -67,15 +45,15 @@ function checkLastDigit($cnp) {
         if($n < 12) {
             $sum += $cnp[$n] * $calc[$n];
         } else {
-            $checker = $cnp[$n];
+            $lastDigit = $cnp[$n];
         }
     }
     
     $res = $sum % intval('11');
     
-    if($res==10 && $checker == 1) {
+    if($res==10 && $lastDigit == 1) {
         return true;
-    } elseif($res == $checker) {
+    } elseif($res == $lastDigit) {
         return true;
     } else {
         return false;
@@ -86,29 +64,57 @@ function checkLastDigit($cnp) {
 function checkBirth($cnp) {
     $raw_cnp = str_split($cnp);
 
-    // [ ] TODO: check if year, month and day is valid date
-    // [ ] TODO: check if year correspond with first digit
-
+    // check year < current_year
     $year = $raw_cnp[1].$raw_cnp[2];
-    // $year = date("Y");
+    $year = intval($year);
+    switch( intval($raw_cnp[0]) ) {
+        case 1  : case 2 : { $year += 1900; } break; 
+        case 3  : case 4 : { $year += 1800; } break; 
+        case 5  : case 6 : { $year += 2000; } break;
+        case 7  : case 8 : case 9 : { $year += 2000;} break;
+        default : { return false; } break;
+    }
+    $yearBool = $year <= date("Y");
+
+    // check month < 12
     $month = $raw_cnp[3].$raw_cnp[4];
-    // $month = $month <= 12;
+    $month = intval($month);
+    $monthBool = $month <= 12;
+
+    // check month < current month if current_year
+    if($year == date("Y")) {
+        if($month > date('m')) {
+            $monthBool = false;
+        }
+    }
+
+    // check day < days of month
     $day = $raw_cnp[5].$raw_cnp[6];
-    // $day = $day <= 31; 
-    // return $year.' '.$month.' '.$day;
-    return false;
+    $day = intval($day);
+    $dayMax = cal_days_in_month(CAL_GREGORIAN, $month, $year);
+    $dayBool = $day <= $dayMax;
+
+    // check day < current day if current_year && current_month
+    if($year == date("Y") and $month == date("m")) {
+        if($day > date('d')) {
+            $dayBool = false;
+        }
+    }
+
+    return $dayBool and $monthBool and $yearBool;
 }
 
 function checkCounty($cnp) {
-    // [ ] TODO: check if number for county is 1 <= x <= 52
-    return false;
+    $raw_cnp = str_split($cnp);
+    $county = $raw_cnp[7].$raw_cnp[8];
+    $county = intval($county);
+    $county = ($county <= 46 && $county > 0) || $county == 51 || $county == 52;
+    return $county;
 }
 
-
-
 // Usage
-echo "<p>CNP: ".$_POST["cnp"]."</p>";
-cnpValidator($_POST["cnp"]);
+// echo "<p>CNP: ".$_POST["cnp"]."</p>";
+echo (cnpValidator($_POST["cnp"])==1 ? 1 : 0);
 
 ?>
 
